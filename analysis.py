910cc8406d2bcf4d23d4d29bbcc334bee417f9e8
jawadghouri <60711395+jawadghouri@users.noreply.github.com>
2026-05-19 02:25:49 +0200

    Removed tempoorary files

--- analysis.py
+++
@@ -1,4 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-
-def compute_similarity(embeddings):
-    return cosine_similarity(embeddings)
--- embedding.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Load model once
-# model = SentenceTransformer('all-MiniLM-L6-v2')
-model = SentenceTransformer('intfloat/e5-small-v2')
-
-def get_embeddings(texts):
-    return model.encode(texts)
