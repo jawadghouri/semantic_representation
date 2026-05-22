c988838efad2fd221c0c4d921c035c9ac9566d24
jawadghouri <60711395+jawadghouri@users.noreply.github.com>
2026-05-22 09:23:32 +0200

    updated bge_embedder

---
+++ embeddings/bge_embedder.py
@@ -0,0 +1,23 @@
+from sentence_transformers import SentenceTransformer
+
+from embeddings.base_embedder import BaseEmbedder
+
+
+class BGEEmbedder(BaseEmbedder):
+
+    def __init__(self):
+
+        self.model = SentenceTransformer(
+            "BAAI/bge-small-en-v1.5"
+        )
+
+    def encode(self, texts):
+
+        embeddings = self.model.encode(
+            texts,
+            batch_size=32,
+            show_progress_bar=True,
+            normalize_embeddings=True
+        )
+
+        return embeddings
